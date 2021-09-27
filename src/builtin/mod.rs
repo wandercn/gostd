5f913dac480221e8b9f5b8a0b15ddac0af37e1c7
wander <wander@ffactory.org>
2021-09-27 13:25:52 +0800

    修复docs.rs文档显示问题，把中文注释放到+号隐藏

--- src/builtin/mod.rs
+++ src/builtin/mod.rs
@@ -3,12 +3,11 @@
 #![allow(unused)]
 
 //! Package builtin binds the basic type in go through the type alias, and implements the basic type cast macro function.
-//! <details>
-//! <summary>zh-cn</summary>
+//! <details class="rustdoc-toggle top-doc">
+//! <summary class="docblock">zh-cn</summary>
 //! builtin 包通过类型别名绑定Go中的基础类型，并实现了基础类型强制转换宏函数。
 //! </details>
 //!
-
 // prelude
 // 导出宏函数 byte!(),int8!()等.避免用gostd::byte!()，可以直接byte!()调用。
 pub use super::{
@@ -17,70 +16,71 @@ pub use super::{
 };
 
 /// Go: uint8 type eq Rust: u8 .
-///
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的uint8类型 等价于 Rust的u8 。
 /// </details>
 pub type uint8 = u8;
 
 /// Go: uint16 type eq Rust: u16
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的uint16类型 等价于 Rust的u16
 /// </details>
 pub type uint16 = u16;
 
 /// Go: uint32 type eq Rust: u32
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的uint32类型 等价于 Rust的u32
 /// </details>
 pub type uint32 = u32;
 
 /// Go: uint64 type eq Rust: u64
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的uint64类型 等价于 Rust的u64
 /// </details>
 pub type uint64 = u64;
 
 /// Go: int8 type eq Rust: i8
-/// <details>
-/// <summary>zh-cn</summary>
-/// Go中的int8类型 等价于 Rust的i8
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
+// Go中的int8类型 等价于 Rust的i8
 /// </details>
 pub type int8 = i8;
 
 /// Go: int16 type eq Rust: i16
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的int16类型 等价于 Rust的i16
 /// </details>
 pub type int16 = i16;
 
 /// Go: int32 type eq Rust: i32
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的int32类型 等价于 Rust的i32
 /// </details>
 pub type int32 = i32;
 
 /// Go: int64 type eq Rust: i64
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的int64类型 等价于 Rust的i64
 /// </details>
 pub type int64 = i64;
 
 /// Go: float32 type eq Rust: float32
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的float32类型 等价于 Rust的 float32
 /// </details>
 pub type float32 = f32;
 
 /// Go: float64 type eq Rust: float64
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的float64类型 等价于 Rust的 float64
 /// </details>
 pub type float64 = f64;
@@ -89,43 +89,43 @@ pub type float64 = f64;
 // type complex128 is no implementation
 
 /// Go: string type eq Rust: String
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的string类型 等价于 Rust的 String
 /// </details>
 pub type string = String;
 
 /// Go: int type eq Rust: isize
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的int类型 等价于 Rust的 isize,但是rust中数组和切片中的索引都是usize，相当于Go中的uint。
 /// </details>
 pub type int = isize;
 
 /// Go: uint type eq Rust: usize
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的uint类型 等价于 Rust的 usize,rust中数组和切片中的索引都是uint。
 /// </details>
 pub type uint = usize;
 
 /// Go: uintptr eq Rust: usize
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的uintptr类型 等价于 Rust的 usize
 /// </details>
 pub type uintptr = usize;
 
 /// Go: byte eq Rust: u8
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的byte类型 等价于 Rust的 u8
 /// </details>
 pub type byte = u8;
 
 /// Go: rune eq Rust: u32
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// Go中的rune类型 等价于 Rust的 u32
 /// </details>
 pub type rune = u32;
@@ -135,8 +135,8 @@ pub const iota: int32 = 0;
 // type error = std::error::Error;
 //
 /// uint8!() eq Go: uint8()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// uint8!() 等价于 Go中的uint8()
 /// </details>
 ///
@@ -160,8 +160,8 @@ macro_rules! uint8 {
 }
 
 /// uint16!() eq Go: uint16()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// uint16!() 等价于 Go中的uint16()
 /// </details>
 ///
@@ -185,8 +185,8 @@ macro_rules! uint16 {
 }
 
 /// uint32!() eq Go: uint32()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// uint32!() 等价于 Go中的uint32()
 /// </details>
 ///
@@ -210,8 +210,8 @@ macro_rules! uint32 {
 }
 
 /// uint64!() eq Go: uint64()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// uint64!() 等价于 Go中的uint64()
 /// </details>
 ///
@@ -235,8 +235,8 @@ macro_rules! uint64 {
 }
 
 /// int8!() eq Go: int8()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// int8!() 等价于 Go中的int8()
 /// </details>
 ///
@@ -260,8 +260,8 @@ macro_rules! int8 {
 }
 
 /// int16!() eq Go: int16()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// int16!() 等价于 Go中的int16()
 /// </details>
 ///
@@ -285,8 +285,8 @@ macro_rules! int16 {
 }
 
 /// int32!() eq Go: int32()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// int32!() 等价于 Go中的int32()
 /// </details>
 ///
@@ -310,8 +310,8 @@ macro_rules! int32 {
 }
 
 /// int64!() eq Go: int64()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// int64!() 等价于 Go中的int64()
 /// </details>
 ///
@@ -335,8 +335,8 @@ macro_rules! int64 {
 }
 
 /// float32!() eq Go: float32()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// float32!() 等价于 Go中的float32()
 /// </details>
 ///
@@ -360,8 +360,8 @@ macro_rules! float32 {
 }
 
 /// float64!() eq Go: float64()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// float64!() 等价于 Go中的float64()
 /// </details>
 ///
@@ -385,8 +385,8 @@ macro_rules! float64 {
 }
 
 /// int!() eq Go: int()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// int!() 等价于 Go中的int()
 /// </details>
 ///
@@ -410,8 +410,8 @@ macro_rules! int {
 }
 
 /// uint!() eq Go: uint()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// uint!() 等价于 Go中的uint()
 /// </details>
 ///
@@ -435,8 +435,8 @@ macro_rules! uint {
 }
 
 /// uintptr!() eq Go: uintptr()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// uintptr!() 等价于 Go中的uintptr()
 /// </details>
 ///
@@ -460,8 +460,8 @@ macro_rules! uintptr {
 }
 
 /// byte!() eq Go: byte()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// byte!() 等价于 Go中的byte()
 /// </details>
 ///
@@ -485,8 +485,8 @@ macro_rules! byte {
 }
 
 /// rune!() eq Go: rune()
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// rune!() 等价于 Go中的rune()
 /// </details>
 ///
@@ -510,9 +510,8 @@ macro_rules! rune {
 }
 
 /// string() eq Go: string(), Convert []byte to string .
-///
-/// <details>
-/// <summary>zh-cn</summary>
+/// <details class="rustdoc-toggle top-doc">
+/// <summary class="docblock">zh-cn</summary>
 /// string() 等价于 Go中的string(),把 []byte数组转换成字符串
 /// </details>
 ///
