ecf4d12cb56a96e6f55d5f36ebd2380ed932c346
wander <wander@ffactory.org>
2024-06-25 10:34:54 +0800

    update

--- time/src/sys/windows.rs
+++ time/src/sys/windows.rs
@@ -1,22 +1,21 @@
+use cvt::cvt;
 use gostd_builtin::*;
 use std::mem;
 use winapi::shared::minwindef::FILETIME;
+use winapi::shared::ntdef::LARGE_INTEGER;
 use winapi::um::profileapi::{QueryPerformanceCounter, QueryPerformanceFrequency};
 use winapi::um::sysinfoapi::GetSystemTimePreciseAsFileTime;
 
 #[cfg(windows)]
 pub fn monotonic_now() -> uint64 {
-    let mut frequency: int64 = 0;
-    let mut counter: int64 = 0;
+    let mut frequency: LARGE_INTEGER = unsafe { mem::zeroed() };
+    let mut counter: LARGE_INTEGER = unsafe { mem::zeroed() };
 
-    unsafe {
-        QueryPerformanceFrequency(&mut frequency);
-        QueryPerformanceCounter(&mut counter);
-    }
-
-    let frequency = uint64!(frequency);
-    let counter = uint64!(counter);
-    let nanoseconds = counter * 1_000_000_000 / frequency;
+    cvt(unsafe { QueryPerformanceFrequency(&mut frequency as *mut _) }).unwrap();
+    cvt(unsafe { QueryPerformanceCounter(&mut counter as *mut _) }).unwrap();
+    let frequency_u64 = uint64!(unsafe { *frequency.QuadPart() });
+    let counter_u64 = uint64!(unsafe { *counter.QuadPart() });
+    let nanoseconds = counter_u64 * 1_000_000_000 / frequency_u64;
 
     uint64!(nanoseconds)
 }
